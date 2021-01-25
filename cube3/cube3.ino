2a77f36497e0a003672cb1b7c3be35119709d1d2
yoshiko <kulala.mbili@gmail.com>
2021-01-25 15:30:17 +0900

    LEDの座標決まってうれちぃ

--- cube3/cube3.ino
+++ cube3/cube3.ino
@@ -5,8 +5,6 @@
 #define SER     (11)    // SPI:MOSI
 
 int c = 1;
-int c1 = 4;
-int c2 = 8;
 
 void setup() {
   pinMode(SRCLK, OUTPUT);
@@ -23,9 +21,6 @@ static int pika[]={
   0b00001000,
   0b00010000,
   0b00100000,
-};
-
-const uint8_t PATTERNS[] = {
   0b00000010,
   0b00000100,
   0b00001000,
@@ -33,28 +28,27 @@ const uint8_t PATTERNS[] = {
   0b00100000,
 };
 
-
 void loop() {
   c++;
-  if (c > 5)c = 0;
-  //いしゅお
+  if (c > 4)c = 0;
+  //hygvgvfrf
   for (int i = 0; i < 2; i++) { //LED_GND
     SPI.transfer(0b11111111);
     digitalWrite(RCLK,  LOW);
     digitalWrite(RCLK,  HIGH);
   }
   for (int i = 0; i < 5; i++) { //LED_RED
-    SPI.transfer(0);
+    SPI.transfer(pika[c]);
     digitalWrite(RCLK,  LOW);
     digitalWrite(RCLK,  HIGH);
   }
   for (int i = 0; i < 5; i++) { //LED_BLUE
-    SPI.transfer(PATTERNS[c]);
+    SPI.transfer(pika[c+1]);
     digitalWrite(RCLK,  LOW);
     digitalWrite(RCLK,  HIGH);
   }
   for (int i = 0; i < 5; i++) { //LED_GREEN
-    SPI.transfer(0);
+    SPI.transfer(pika[c+2]);
     digitalWrite(RCLK,  LOW);
     digitalWrite(RCLK,  HIGH);
   }

a74029a0771d5d88a9274da260b5c34e30ad893b
yoshiko <kulala.mbili@gmail.com>
2021-01-27 16:36:17 +0900

    0127

--- cube4/cube4.ino
+++ cube4/cube4.ino
@@ -11,15 +11,15 @@ void interrupt() {
 void setup() {
   MsTimer2::set(50, interrupt);
   MsTimer2::start();
-  ene.init(2);//ボード番号2で開始
+  ene.init(1);
 }
 
 void loop() {
   c++;
-  //  ene.led_center();
-  //  delay(1000);
-  //  ene.led_xax(c%3);
-  //  ene.led_yax(c%3);
-  //  ene.led_zax(c%3);
-  ene.led_test(c % 3);
+    ene.led_center();
+    delay(200);
+    ene.led_xax(c%3);
+    ene.led_yax(c%3);
+    ene.led_zax(c%3);
+  //ene.led_test(c % 3);
 }
